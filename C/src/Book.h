20691b5b2ddd3ed2a0a1ef0f198dada9427ddf6f
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-09-11 22:52:07 +0530

    tu

--- C/src/Book.h
+++ C/src/Book.h
@@ -1,21 +1,33 @@
-///@file Book.h
-///@page Book header
+/// @file Book.h
+/// @brief This file contains the definitions and functions for managing books in the library system.
 
-#ifndef BOOK.h
-#define BOOK.h
+#ifndef BOOK_H
+#define BOOK_H
 
-typedef struct{
-    char name[100];
-    char author[100];
-    char volume[100];
-    char issued[100];
+/// @brief Structure to represent a book.
+typedef struct {
+    char name[100];   ///< Name of the book.
+    char author[100]; ///< Author of the book.
+    char volume[100]; ///< Volume of the book.
+    char issued[100]; ///< Issued status of the book.
 } Book;
 
+/// @brief Adds a new book to the library.
+/// @param book Pointer to the Book structure.
+/// @param name Name of the book.
+/// @param author Author of the book.
+/// @param volume Volume of the book.
+void add_book(Book *book, const char* name, const char* author, const char* volume);
 
-void add_book(Book *book, const char* name,const char* author,const char* volume);
+/// @brief Retrieves all books in the library.
 void get_all_books();
+
+/// @brief Issues a book to a member.
+/// @param name Name of the book to be issued.
 void issue_book(const char* name);
+
+/// @brief Returns a book to the library.
+/// @param name Name of the book to be returned.
 void return_book(const char* name);
 
-
-#endif
+#endif // BOOK_H
