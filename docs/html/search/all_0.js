8307c4ede06cd49893e954907f872e0af81a8238
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-09-12 14:54:47 +0530

    TY

--- docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['library_2dmanagement_2dsystem_2dc_0',['library-management-system-c',['../md_README.html',1,'']]]
-];
--- docs/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['library_2dmanagement_2dsystem_2dc_1',['library-management-system-c',['../md_README.html',1,'']]]
-];
